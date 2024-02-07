d26d0dac156b4b908c2ef30192db7e0d9e1d385c
arthkim <artur@DESKTOP-VMR2FQM>
2024-02-07 12:10:11 +0600

    add: name of users

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"net"
 	"os"
+	"strings"
 
 	nc "nc/pkg/logger"
 )
 
 var messages = make(chan string)
-var clients []net.Conn
+var clients []*Client
+
+type Client struct {
+	conn net.Conn
+	name string
+}
 
 func main() {
 	lg := nc.NewLogger()
@@ -40,11 +47,27 @@ func main() {
 		clientAddr := conn.RemoteAddr().String()
 		lg.InfoLog.Printf("Client connected: %s\n", clientAddr)
 
-		// Добавляем клиента в список
-		clients = append(clients, conn)
+		// Спросить имя у клиента
+		conn.Write([]byte("Enter your name: "))
+		name, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			lg.ErrorLog.Println("Error reading client name:", err)
+			continue
+		}
+		name = strings.TrimSpace(name)
+
+		// Создать объект клиента и добавить его в список
+		client := &Client{
+			conn: conn,
+			name: name,
+		}
+		clients = append(clients, client)
+
+		// Приветствие нового клиента
+		conn.Write([]byte("Welcome to the chat, " + name + "!\n"))
 
 		// Обработка клиента в отдельной горутине
-		go handleConnection(conn)
+		go handleConnection(client)
 	}
 }
 
@@ -56,30 +79,24 @@ func broadcastMessages() {
 
 		// Отправляем сообщение всем клиентам
 		for _, client := range clients {
-			client.Write([]byte(message))
+			client.conn.Write([]byte(message))
 		}
 	}
 }
 
 // Функция для обработки подключения клиента
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	// Приветствие нового клиента
-	conn.Write([]byte("Welcome to the chat!\n"))
+func handleConnection(client *Client) {
+	defer client.conn.Close()
 
 	// Бесконечный цикл для чтения сообщений от клиента
-	scanner := bufio.NewScanner(conn)
+	scanner := bufio.NewScanner(client.conn)
 	for scanner.Scan() {
 		message := scanner.Text()
 
-		if message == "" {
-			continue
-		} else {
-			message = message + "\n"
-		}
+		// Формируем сообщение с именем отправителя
+		messageWithSender := fmt.Sprintf("[%s]: %s\n", client.name, message)
 
 		// Отправляем сообщение в канал для рассылки другим клиентам
-		messages <- message
+		messages <- messageWithSender
 	}
 }
